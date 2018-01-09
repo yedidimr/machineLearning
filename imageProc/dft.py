bda059dd14ec2dfc2ad06ed88c02d74af2f586e6
Reut <yedidimr@gmail.com>
2018-01-09 16:14:08 +0200

    fix fft

--- imageProc/dft.py
+++ imageProc/dft.py
@@ -1,118 +1,78 @@
 import numpy as np
 import matplotlib.pylab as plt
-from scipy import misc
+import cv2
+
+image_path1 = "/home/reut/Downloads/apple.jpg"
+image_path2 = "/home/reut/Downloads/raccoon.png"
+
+plot_i = 0
+def plot(img, title, n_rows=4, n_cols=2):
+    global plot_i
+    assert plot_i < n_rows * n_cols
+    plot_i += 1
+    plt.subplot(n_rows, n_cols, plot_i)
+    plt.imshow(img, cmap='gray')
+    plt.title(title)
+    plt.xticks([])
+    plt.yticks([])
 
 
-image_path1 = "/home/student-5/Downloads/chess.png"
-image_path2 = "/home/student-5/Downloads/apple2.png.png"
+def extract(image):
+   """
+   returns the magnitude and phase according to FFT
+   """
+   # calculate fft
+   spectrum = np.fft.fft2(image)
+   fshift = np.fft.fftshift(spectrum)  # to make the magnitude graph with the lower frequency in the middle
 
-# image_path1 = "/home/student-5/Downloads/face.png"
-# image_path2 = "/home/student-5/Downloads/aple.jpeg"
+   # calculate phase and magnitude
+   magnitude = np.abs(fshift)
+   phase = np.angle(fshift)
 
-# read images
-img = misc.imread(image_path1, flatten=True)
-img2 = misc.imread(image_path2, flatten=True)
+   return magnitude, phase
 
-#resize by the smaller image
-if (img2.size < img.size):
-    img = misc.imresize(img, img2.shape )
-if (img.size < img2.size):
-    img2 = misc.imresize(img2, img.shape )
 
-# calculate fft
-spectrum = np.fft.fft2(img)
-fshift = np.fft.fftshift(spectrum)  # to make the magnitude graph with the lower frequency in the middle
+def constract(phase, magnitude):
+   """
+   constrat an image according to it's phase and magnitude using inverse FFT
+   """
+   new_spectrum = magnitude * np.exp(1j * phase)
 
-spectrum2 = np.fft.fft2(img2)
-fshift2 = np.fft.fftshift(spectrum2)
+   # reverse the shift and FFT
+   f_ishift = np.fft.ifftshift(new_spectrum)
+   img_back = np.fft.ifft2(f_ishift)
+   
+   return np.abs(img_back)
 
-# calculate phase and magnitude
-magnitude = np.abs(fshift)
-phase = np.angle(fshift)
 
-magnitude2 = np.abs(fshift2)
-phase2 = np.angle(fshift2)
 
+img = cv2.imread(image_path1, 0)
+img2 = cv2.imread(image_path2, 0)
+
+
+# extract magnitude & phase from each image
+magnitude, phase= extract(img)
+magnitude2, phase2 = extract(img2)
 
 # create a new image out of different magnitude and phase:
-
-# calculate spectrum from magnitude and phase
-new_spectrum = magnitude2 * np.exp(1j * phase)
-new_spectrum2 = magnitude * np.exp(1j * phase2)
-
-# reverse the shift and FFT
-f_ishift = np.fft.ifftshift(new_spectrum)
-img_back = np.fft.ifft2(f_ishift)
-img_back = np.abs(img_back)
-
-f_ishift2 = np.fft.ifftshift(new_spectrum2)
-img_back2 = np.fft.ifft2(f_ishift2)
-img_back2 = np.abs(img_back2)
-
-# plot
-
-plt.subplot(421)
-plt.imshow(img, cmap='gray')
-plt.title('Input Image1')
-plt.xticks([])
-plt.yticks([])
-
-plt.subplot(422)
-plt.imshow(img2, cmap='gray')
-plt.title('Input Image2')
-plt.xticks([])
-plt.yticks([])
+img_back = constract(phase, magnitude2)
+img_back2 = constract(phase2, magnitude)
 
 
-# show magnitude
-plt.subplot(423)
-magnitude_spectrum = 20*np.log(np.abs(fshift)) # log must be done to show correctly
-plt.imshow(magnitude_spectrum, cmap='gray')
-plt.title('Magnitude Spectrum of img1')
-plt.xticks([])
-plt.yticks([])
-
-plt.subplot(424)
-magnitude_spectrum2 = 20*np.log(np.abs(fshift2)) # log must be done to show correctly
-plt.imshow(magnitude_spectrum2, cmap='gray')
-plt.title('Magnitude Spectrum of img2')
-plt.xticks([])
-plt.yticks([])
+# prepare magniture to display - take the log to make magnitude visable on heatmap
+visible_magnitude = 20*np.log(magnitude) 
+visible_magnitude2 = 20*np.log(magnitude2) 
 
 
-# show phase
-plt.subplot(425)
-plt.imshow(phase, cmap='gray')
-plt.title('Phase Spectrum img 1')
-plt.xticks([])
-plt.yticks([])
+# display output
+plot_i = 0
+plot(img, 'Input img 1')
+plot(img2, 'Input img 2')
+plot(visible_magnitude, 'Magnitude Spectrum img 1')
+plot(visible_magnitude2, 'Magnitude Spectrum img 2')
 
-plt.subplot(426)
-plt.imshow(phase2, cmap='gray')
-plt.title('Phase Spectrum img 2')
-plt.xticks([])
-plt.yticks([])
-
-
-plt.subplot(427)
-plt.imshow(img_back, cmap='gray')
-plt.title('New img - phase of img1')
-plt.xticks([])
-plt.yticks([])
-
-plt.subplot(428)
-plt.imshow(img_back2, cmap='gray')
-plt.title('New img - phase of img2')
-plt.xticks([])
-plt.yticks([])
-
+plot(phase, 'Phase Spectrum img 1')
+plot(phase2, 'Phase Spectrum img 2')
+plot(img_back, 'New img - phase of img1')
+plot(img_back2, 'New img - phase of img2')
 plt.show()
-
-
-
-
-
-
-
-
-
